f6394106bdaf6a7d497bfa924b96ff2561a2a6c1
Denis Bogdanenko <shiirochi@yandex.ru>
2019-12-22 13:41:27 +0300

    apply code style

--- src/components/WaveForm.js
+++ src/components/WaveForm.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 const WaveForm = ({ id, children }) => {
-    return <div id={id}>{children}</div>;
+  return <div id={id}>{children}</div>;
 };
 
 WaveForm.defaultProps = {
-    waveColor: "violet",
-    progressColor: "purple",
-    id: "waveform"
+  waveColor: "violet",
+  progressColor: "purple",
+  id: "waveform"
 };
 
 export default WaveForm;
