a7e8a764ed2344f0218e2f39e9c05b2559bb6c26
Denis Bogdanenko <shiirochi@yandex.ru>
2020-02-15 17:53:18 +0300

    create getDifference utility function

---
+++ src/utils/getDifference.js
@@ -0,0 +1,17 @@
+const getDifference = (arr1, arr2) => {
+  let nextArr1 = [...arr1];
+  let nextArr2 = [...arr2];
+
+  const disabled = nextArr1.filter(item => {
+    return nextArr2.findIndex(nextItem => nextItem.name === item.name) === -1;
+  });
+
+  const enabled = nextArr2.filter(item => {
+    return nextArr1.findIndex(nextItem => nextItem.name === item.name) === -1;
+  });
+
+  return { disabled, enabled };
+  // map arr2 via reduce and remove each item, which name exists in arr1
+};
+
+export default getDifference;
