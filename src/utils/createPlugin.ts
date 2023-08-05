0f31d37533bea6fe4886d9937c05cc93b8001279
Denis Bogdanenko <shiirochi@yandex.ru>
2023-08-05 17:17:45 +0300

    [ref] refactor creator existence check

--- src/utils/createPlugin.ts
+++ src/utils/createPlugin.ts
@@ -7,9 +7,9 @@ export default function createPlugin(pluginObj: PluginType): PluginDefinition {
   const createMethod: any = plugin[creator as keyof typeof plugin];
 
   if (!plugin) throw new Error(`Please pass a valid plugin in plugin list`);
-  if(!creator) throw new Error(`Please pass the creator function name in 'creator' property.`)
+  if (!creator) throw new Error(`Please pass the creator function name in 'creator' property.`)
 
-  if (createMethod instanceof Function === false) {
+  if (typeof createMethod !== 'function') {
     throw new Error(`"${creator}" is not callable on given plugin. Please pass a valid 'creator' in plugins list.`)
   }
 
