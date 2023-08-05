c0f83b9552e39a2f6a7b1ae4baae7104ed12c0d7
Denis Bogdanenko <shiirochi@yandex.ru>
2023-08-05 17:20:22 +0300

    [ref] update getDifference

--- src/utils/getDifference.ts
+++ src/utils/getDifference.ts
@@ -1,16 +1,12 @@
 import { PluginDefinition } from "wavesurfer.js/types/plugin";
 
 const getDifference = (arr1: PluginDefinition[], arr2: PluginDefinition[]) => {
-  let nextArr1 = [...arr1];
-  let nextArr2 = [...arr2];
+  const nextArr1 = [...arr1];
+  const nextArr2 = [...arr2];
 
-  const disabled = nextArr1.filter(item => {
-    return nextArr2.findIndex((nextItem) => nextItem.name === item.name) === -1;
-  });
+  const disabled = nextArr1.filter(item => nextArr2.findIndex((nextItem) => nextItem.name === item.name) === -1);
 
-  const enabled = nextArr2.filter(item => {
-    return nextArr1.findIndex(nextItem => nextItem.name === item.name) === -1;
-  });
+  const enabled = nextArr2.filter(item => nextArr1.findIndex(nextItem => nextItem.name === item.name) === -1);
 
   return { disabled, enabled };
   // map arr2 via reduce and remove each item, which name exists in arr1
