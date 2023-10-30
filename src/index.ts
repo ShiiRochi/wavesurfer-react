cc32d9550ef5b8e2746ec58146bb42ec6589df49
Denis Bogdanenko <shiirochi@yandex.ru>
2023-10-30 22:40:24 +0300

    v3(ref): remove Marker component

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import Region from "./components/Region";
-import Marker from "./components/Marker";
 import WaveSurfer from "./containers/WaveSurfer";
 import WaveForm from "./components/WaveForm";
 import useWavesurfer from "./hooks/useWavesurfer";
 import useWavesurferContext from "./hooks/useWavesurferContext";
 
-export { Region, WaveSurfer, WaveForm, Marker, useWavesurfer, useWavesurferContext };
+export { Region, WaveSurfer, WaveForm, useWavesurfer, useWavesurferContext };
