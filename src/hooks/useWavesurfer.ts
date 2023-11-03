6770ec4da641038e2c61be7e0fd527d8037fbf8b
Denis Bogdanenko <shiirochi@yandex.ru>
2023-11-03 11:07:46 +0300

    v3(ref): update hooks

--- src/hooks/useWavesurfer.ts
+++ src/hooks/useWavesurfer.ts
@@ -1,24 +1,24 @@
 import {useEffect, useRef, useState} from "react";
-import { PluginDefinition } from "wavesurfer.js/types/plugin";
+import { GenericPlugin } from "wavesurfer.js/dist/base-plugin";
 import createWavesurfer, {WaveSurfer as WaveSurferRef, WaveSurfer} from "../utils/createWavesurfer";
 import createPlugin from "../utils/createPlugin";
 import getDifference from "../utils/getDifference";
 import { PluginType } from "../types";
 
-type UseWaveSurferParams = {
+type UseWaveSurferParams<GPlug extends GenericPlugin> = {
     container?: string | HTMLElement,
-    plugins: PluginType[],
+    plugins: PluginType<GPlug>[],
     onMount: (wavesurferRef: null | WaveSurferRef) => any
 };
 
-export default function useWavesurfer({ container, plugins = [], onMount, ...props }: UseWaveSurferParams) {
-    const usedPluginsListCache = useRef<PluginDefinition[]>([]);
+export default function useWavesurfer<GPlug extends GenericPlugin>({ container, plugins = [], onMount, ...props }: UseWaveSurferParams<GPlug>) {
+    const usedPluginsListCache = useRef<GPlug[]>([]);
     const [wavesurfer, setWavesurfer] = useState<WaveSurfer | null>(null);
 
     useEffect(() => {
         if (!container) return;
 
-        let _plugins: PluginDefinition[] = [];
+        let _plugins: GPlug[] = [];
         // construct initial plugins list
 
         if (plugins) {
@@ -48,7 +48,7 @@ export default function useWavesurfer({ container, plugins = [], onMount, ...pro
     // TODO: think about whether its place is this hook?
     useEffect(() => {
         if (wavesurfer) {
-            const nextPluginsMap = plugins.map(createPlugin);
+            const nextPluginsMap = plugins.map(p => createPlugin(p));
 
             const { disabled, enabled } = getDifference(
                 usedPluginsListCache.current,
@@ -57,15 +57,15 @@ export default function useWavesurfer({ container, plugins = [], onMount, ...pro
 
             usedPluginsListCache.current = nextPluginsMap;
 
-            disabled.forEach((plugin) => {
-                if (!plugin.name) return;
-                wavesurfer?.destroyPlugin(plugin.name);
-            });
+            const activePlugins = wavesurfer?.getActivePlugins();
 
-            enabled.forEach((plugin) => {
-                if (!plugin.name) return;
-                wavesurfer?.addPlugin(plugin).initPlugin(plugin.name);
-            });
+
+            // destroy plugin, wavesurfer self removes it from plugin array
+            disabled
+              .map(plug => activePlugins?.find(p => p === plug))
+              .forEach(plug => {plug?.destroy();})
+
+            enabled.forEach((plugin) => wavesurfer?.registerPlugin(plugin));
         }
     }, [plugins]);
 
