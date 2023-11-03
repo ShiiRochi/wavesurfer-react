7e0dffce0e0b622bcc67097601f69a3166687c9a
Denis Bogdanenko <shiirochi@yandex.ru>
2023-11-03 11:09:17 +0300

    v3(ref): update types

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,10 @@
-export interface PluginType {
-    plugin: object;
+import { GenericPlugin } from "wavesurfer.js/dist/base-plugin";
+import { GeneralEventTypes } from "wavesurfer.js/dist/event-emitter";
+
+export interface PluginType<GPlug extends GenericPlugin> {
+    plugin: GPlug;
     options: any;
     creator?: string;
 }
+
+export type EventListener<EventTypes extends GeneralEventTypes, EventName extends keyof EventTypes> = (...args: EventTypes[EventName]) => void;
