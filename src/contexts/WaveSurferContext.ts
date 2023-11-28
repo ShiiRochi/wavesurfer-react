d1756015f55f93c47a7f30ea447b13d5177c6a4f
Denis Bogdanenko <shiirochi@yandex.ru>
2023-11-28 19:47:34 +0300

    v3(ref): update WaveSurferContext

--- src/contexts/WaveSurferContext.ts
+++ src/contexts/WaveSurferContext.ts
@@ -3,8 +3,6 @@ import WaveSurfer from 'wavesurfer.js';
 import { GenericPlugin } from "wavesurfer.js/dist/base-plugin";
 import { PluginDictionary } from "../hooks/useWavesurfer";
 
-export type WaveSurferContextValue = readonly [WaveSurfer, PluginDictionary<GenericPlugin>, GenericPlugin[]];
-
-const WaveSurferContext = createContext<WaveSurferContextValue | null>(null);
+const WaveSurferContext = createContext<readonly [WaveSurfer, PluginDictionary<GenericPlugin>, GenericPlugin[]] | null>(null);
 
 export default WaveSurferContext;
